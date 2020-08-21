a517eadb19421dc792f57868b221b630ed805ae1
wangsaisai <shanbencilang@gmail.com>
2020-08-22 02:20:52 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import './plugins/element.js'
+import './assets/css/global.css'
+// 导入全局样式表
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+Vue.prototype.$http = axios
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 Vue.config.productionTip = false
 
